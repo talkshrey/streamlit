3bee681283ba6efb7b5cc3cbe7041f489805a3d3
talkshrey <talkshrey@gmail.com>
2023-07-21 13:06:49 +0530

    environment variables added

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import openai
 import streamlit as st
 from streamlit_chat import message
+import os
 
 st.set_page_config(page_title="FinBot", page_icon=":robot_face:")
 st.markdown("<h1 style='text-align: center;'>FinBot - Financial chat bot</h1>", unsafe_allow_html=True)
 
-openai.api_key = "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"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Initialise session state variables
 if 'generated' not in st.session_state:
