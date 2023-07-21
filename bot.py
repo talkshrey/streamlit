ed26990c59a9394f2efebf876e5bdd91c289cc18
talkshrey <talkshrey@gmail.com>
2023-07-21 12:50:00 +0530

    Streamlit integration

---
+++ bot.py
@@ -0,0 +1,120 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+st.set_page_config(page_title="FinBot", page_icon=":robot_face:")
+st.markdown("<h1 style='text-align: center;'>FinBot - Financial chat bot</h1>", unsafe_allow_html=True)
+
+openai.api_key = "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"
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", 
+        "content": "You are a financial advisor who helps individuals and families in making financial decisions. \
+            Your company name is Bajaj Finserv and you have your own schemes and systematic plans for investment \
+            and insurances. You will also encourage the user to apply for your plans and buy products on EMI schemes\
+            provided by you. Try to provide efficient methods and strategies for a person based on their current economic scenario \
+            But first you will be required to collect some details from the user based on which you can make \
+            suggestions. You can ask the following questions to the user: age of the user, monthly income range \
+            of the user, any existing financial plans, current budget set on different categories. You can also \
+            provide an option to input budget data from user and create financial advise based on these parameters."
+        }
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'cost' not in st.session_state:
+    st.session_state['cost'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+if 'total_cost' not in st.session_state:
+    st.session_state['total_cost'] = 0.0
+
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+# Map model names to OpenAI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo"
+
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", 
+        "content": "You are a financial advisor who helps individuals and families in making financial decisions. \
+            You can ask the following questions to the user: age of the user, monthly income range of the user, any \
+            existing financial plans, current budget set on different categories. You can also provide an option to \
+            input budget data from user and create financial advise based on these parameters. \
+            Try to provide efficient methods and strategies for a person based on their current economic scenario \
+            But first you will be required to collect some details from the user based on which you can make suggestions."
+        }
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+    st.session_state['cost'] = []
+    st.session_state['total_cost'] = 0.0
+    st.session_state['total_tokens'] = []
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+
+# generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+
+    # print(st.session_state['messages'])
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['model_name'].append(model_name)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+        if model_name == "GPT-3.5":
+            cost = total_tokens * 0.002 / 1000
+        else:
+            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+        st.session_state['cost'].append(cost)
+        st.session_state['total_cost'] += cost
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
